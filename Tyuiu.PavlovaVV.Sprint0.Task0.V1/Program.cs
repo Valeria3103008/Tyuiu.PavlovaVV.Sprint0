354024da833c4b6cbc9c9b8bcf28ab86a114e140
leroc <leroc@LAPTOP-9S0RDN55>
2025-09-08 18:10:51 +0500

    Add project files.

---
+++ Tyuiu.PavlovaVV.Sprint0.Task0.V1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.PavlovaVV.Sprint0.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
