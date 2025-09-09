ac2dbabe1a66ab3351228c1b414d4bc9b41e1b0f
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-09-09 21:38:09 +0500

    Task2

---
+++ Tyuiu.PavlovaVV.Sprint0.V1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PavlovaVV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PavlovaVV.Sprint0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Валерия"));
+            Console.ReadKey();
+        }
+    }
+}
